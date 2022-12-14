2221e6446780be4a17e1a329bd919015f93be6fd
Ray Zana <rayzana280@gmail.com>
2022-12-14 14:36:07 -0500

    commit tree a6667690886616e017d9a8168d701bd033ab889f

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  if( 400 >= someValue){
+  return 'This one is on me!'
+  }else if( someValue >= 400 && someValue <= 2000 ){
+    return 'That will be twenty bucks.'
+  }else if( someValue > 2000 && someValue < 2500 ){
+    return 'I will gladly take your thirty bucks.'
+  }else if( someValue > 2500){
+  return 'No can do.'
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go."
+
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      default:
+        return "Bye."
+  }
+
   // Write your code here!
 }
